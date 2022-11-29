35ab8985601b89ae73eb88be14102b241c1030a1
moses <shroomce@gmail.com>
2022-11-30 04:27:10 +0500

    Made queue -> fix segfault next

---
+++ queue/linked_queue/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+
+#include "queue.h"
+
+#define SIZE  10
+#define FROM  33
+#define TO   126
+
+int
+rand_range (int from, int to) {
+	assert (from < to && from >= 0);
+
+	return (from + rand () / (RAND_MAX / (to - from + 1) + 1));
+}
+
+int main (void) {
+	srand (time (NULL));
+
+	struct queue_t *queue;
+	queue = init_queue ();
+
+	printf ("******************** Adding elements to queue ********************\n");
+	int randnum;
+	int ind = 0;
+	while (ind <= SIZE) {
+		randnum = rand_range (FROM, TO);
+		enqueue (queue, randnum);
+		printf ("Added element %c(%d) to queue.\n", randnum);
+	}
+
+	printf ("******************** Removing elements from queue ********************\n");
+	int removed;
+	while ( !is_empty (queue)) {
+		removed = dequeue (queue);
+
+	printf ("Removed element %c(%d) from queue.\n", removed);
+	}
+
+	return (0);
+}
--- queue/linked_queue/queue.c
+++ queue/linked_queue/queue.c
@@ -34,18 +34,62 @@ return_node (struct queue_node_t *qn) {
 
 /*==================== queue manipulation ====================*/
 struct queue_t*
-init_queue (void);
+init_queue (void) {
+	struct queue_t *q;
+	
+	q = (struct queue_t*)malloc (sizeof (struct queue_t));
+	q -> remove = q -> insert = NULL;
+	return (q);
+}
 
 void
-deinit_queue (struct queue_t *q);
+deinit_queue (struct queue_t *q) {
+	struct queue_node_t *temp;
+
+	while (q -> remove != NULL) {
+		temp = q -> remove;
+		q -> remove = temp -> next;
+		return_node (temp);
+	}
+	free (q);
+}
 
 int
-is_empty (struct queue_t *q);
+is_empty (struct queue_t *q) {
+	return (q -> insert == NULL);
+}
 
 void
-enqueue (struct queue_t *q, char item);
+enqueue (struct queue_t *q, char item) {
+	struct queue_node_t *temp;
+
+	temp = get_node ();
+	temp -> item = item;
+	temp -> next = NULL;
+
+	if (q -> insert != NULL) {
+		q -> insert -> next = temp;
+		q -> insert = temp;
+	} else {
+		q -> remove = q -> insert = temp;
+	}
+
+}
 
 char
-dequeue (struct queue_t *q);
+dequeue (struct queue_t *q) {
+	struct queue_node_t *temp;
+	char temp_item;
+
+	temp = q -> remove;
+	temp_item = temp -> item;
+	q -> remove = temp -> next;
+
+	if (q -> remove == NULL) {
+		q -> insert = NULL;
+	}
+	return_node (temp);
+	return (temp_item);
+}
 
 /*==================== end ====================*/
